bbd63bf18293af4f59a5b05577bd30e04cc748eb
sangeonpark <parksang1324@gmail.com>
2022-02-25 20:15:20 +0900

    2/25

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -1,5 +1,74 @@
 var imageViewer = {
-	init: function(){
-		
-	}
+	init: function() {
+		_this = this
+		$(function() {
+			$("#btn-change").click(_this.changeImage.bind(_this));
+
+		}.bind(this))
+	},
+	
+	changeImage: function() {
+		console.log(this)
+		var index = Math.floor(Math.random() * this._images.length);
+		console.log(index)
+		$(".image-viewer img").attr({
+		alt: this._images[index].name,
+		src: 'images/' + this._images[index].file
+		})
+	},
+
+/*	slideShow: function() {
+
+		if ($(this).text() == '슬라이드쇼 시작') {
+			$(this).text('슬라이드 쇼 중지')
+			timer = setInterval(function() {
+				var index = Math.floor(Math.random() * images.length);
+				$(".image-viewer img").attr({
+					alt: images[index].name,
+					src: 'images/' + images[index].file
+				});
+			}, 3000)
+		} else {
+			$(this).text('슬라이드쇼 시작');
+			clearInterval(timer);
+		}
+	},*/
+
+_images: [{
+
+	name : "국화",
+		file : "Chrysanthemum.jpg"
+	}, {
+		name : "사막",
+		file : "Desert.jpg"
+	},
+
+	{
+		name : "수국",
+		file : "Hydrangeas.jpg"
+	},
+
+	{
+		name : "해파리",
+		file : "Jellyfish.jpg"
+	},
+
+	{
+		name : "코알라",
+		file : "Koala.jpg"
+	}, {
+		name : "등대",
+		file : "Lighthouse.jpg"
+	},
+
+	{
+		name : "펭귄",
+		file : "Penguins.jpg"
+	},
+
+	{
+		name : "툴립",
+		file : "Tulips.jpg"
+	} ]
+
 }
---
+++ ch08/src/main/java/com/poscoict/ch08/controller/api/ApiController.java
@@ -0,0 +1,39 @@
+package com.poscoict.ch08.controller.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.poscoict.ch08.controller.dto.JsonResult;
+import com.poscoict.ch08.controller.vo.GuestbookVo;
+
+@Controller
+@RequestMapping("/api")
+public class ApiController {
+	
+	@ResponseBody
+	@RequestMapping("/text")
+	public String text() {
+		return "Text 데이터";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/html")
+	public String html() {
+		return "<h1>AJAX 연습</h1><p>HTML 데이터</p>";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/json", method = RequestMethod.GET)
+	public JsonResult json() {
+		GuestbookVo vo = new GuestbookVo();
+		vo.setNo(1L);
+		vo.setName("둘리");
+		vo.setMessage("호이~");
+		
+//		JsonResult jsonResult = new JsonResult();
+//		jsonResult.setResult("");
+		return JsonResult.success(vo);
+	}
+}
